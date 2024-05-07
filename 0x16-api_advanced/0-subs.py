7dfbf24d70398b98bdbef8b9d02a699a18f3d780
Munoru101 <dennismunoru@gmail.com>
2024-05-07 22:38:51 +0300

    Query Reddit API for number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API and returns
+the number of subscribers for a given subreddit.
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "MyBot/1.0"}  # Set custom User-Agent
+
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
