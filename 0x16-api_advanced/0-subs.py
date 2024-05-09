436d3235a66107ebb5e43b3bb47eb676eb5ebadd
Munoru101 <dennismunoru@gmail.com>
2024-05-10 00:16:05 +0300

    handle non-existing subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
 """
-Queries the Reddit API and returns
-the number of subscribers for a given subreddit.
+Module to query the Reddit API and retrieve the number of
+subscribers for a given subreddit
 """
 
-import requests
+from requests import get
 
 
 def number_of_subscribers(subreddit):
+    """
+    retrieves the number of subscribers for a given subreddit
+    """
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+    user_agent = {'User-Agent': 'CustomUserAgent'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
 
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "MyBot/1.0"}  # Set custom User-Agent
-
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
-    else:
+    try:
+        response = get(url, headers=user_agent, allow_redirects=False)
+        response.raise_for_status()
+        output = response.json()
+        return output.get('data').get('subscribers')
+    except requests.exceptions.HTTPError as http_err:
         return 0
